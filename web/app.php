bc840138d825051e11abeeb9cb74339f75b20ee6
Kostas Milonas <milonas.ko@gmail.com>
2015-10-22 21:55:11 +0000

    changes route parameter name on prod environment's front controller

--- web/app.php
+++ web/app.php
@@ -8,7 +8,7 @@ $config = \json_decode(\file_get_contents("../config/prod.json"), true);
 
 $core = new Core($config);
 $core->map("/hello/{name}", "GrainSkeleton:HelloController:index")
-    ->map("/persons/{name}", "GrainSkeleton:HelloController:showPersons");
+    ->map("/persons/{firstName}", "GrainSkeleton:HelloController:showPersons");
 
 $response = $core->handle(\filter_input(INPUT_SERVER, "REQUEST_URI"));
 echo $response;
