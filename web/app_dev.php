0c6e0321d794b18d3ffde9d077a89fa9e590aebe
Kostas Milonas <milonas.ko@gmail.com>
2016-04-18 17:56:48 +0000

    project is in charge of loading the service definition

--- web/app_dev.php
+++ web/app_dev.php
@@ -5,8 +5,11 @@ require "../vendor/autoload.php";
 use Grain\Core;
 
 $config = \json_decode(\file_get_contents("../config/dev.json"), true);
+$servicesDefinition = \json_decode(\file_get_contents("../src/GrainSkeleton/Resources/services.json"), true);
 
 $core = new Core($config);
+$core->initializeContainer($servicesDefinition);
+
 $core->map("/hello/{name}", "GrainSkeleton:Hello:index")
     ->map("/persons/{firstName}", "GrainSkeleton:Hello:showPersons");
 
