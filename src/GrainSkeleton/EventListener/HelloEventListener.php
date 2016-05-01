75cc72b56b86ad412fefd628cf742938d3673a6b
Kostas Milonas <milonas.ko@gmail.com>
2016-05-01 12:32:37 +0300

    fixes minor misspellings in event listeners output

--- src/GrainSkeleton/EventListener/HelloEventListener.php
+++ src/GrainSkeleton/EventListener/HelloEventListener.php
@@ -6,11 +6,11 @@ class HelloEventListener
 {
     public function onPostRequest()
     {
-        echo "Hello from the post-request event listner.<br />";
+        echo "Hello from the post-request event listener.<br />";
     }
     
     public function onPreResponse()
     {
-        echo "Hello from the pre-response event listner.<br />";
+        echo "Hello from the pre-response event listener.<br />";
     }
 }
